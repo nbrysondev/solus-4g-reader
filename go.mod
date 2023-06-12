4fc0396787b2592ccd85b8bfe315de5f50ad3812
nbrysondev <neil.bryson@gmail.com>
2023-06-12 23:47:40 +0100

    Use a stripped down fork of modbus

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.19
 
 require (
 	github.com/goburrow/serial v0.1.0 // indirect
+	github.com/nbrysondev/modbus-rtu-tinygo v1.0.0 // indirect
 	github.com/simonvetter/modbus v1.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,66 +3,67 @@
 package SolusReader
 
 import (
-    "time"
-    "encoding/json"
-    "github.com/simonvetter/modbus"
+	"encoding/json"
+	"time"
+
+	"github.com/nbrysondev/modbus-rtu-tinygo"
 )
 
 func GetSolusReadings() string {
-    var client  *modbus.ModbusClient
-    var err      error
+	var client *modbus.ModbusClient
+	var err error
 
-    // for an RTU (serial) device/bus
-    client, err = modbus.NewClient(&modbus.ClientConfiguration{
-        URL:      "rtu:///dev/ttyUSB0",
-        Speed:    9600,                   // default
-        DataBits: 8,                       // default, optional
-        Parity:   modbus.PARITY_NONE,      // default, optional
-        StopBits: 1,                       // default if no parity, optional
-        Timeout:  300 * time.Millisecond,
-    })
+	// for an RTU (serial) device/bus
+	client, err = modbus.NewClient(&modbus.ClientConfiguration{
+		URL:      "rtu:///dev/ttyUSB0",
+		Speed:    9600,               // default
+		DataBits: 8,                  // default, optional
+		Parity:   modbus.PARITY_NONE, // default, optional
+		StopBits: 1,                  // default if no parity, optional
+		Timeout:  300 * time.Millisecond,
+	})
 
-    if err != nil {
-        // error out if client creation failed
-    }
+	if err != nil {
+		// error out if client creation failed
+	}
 
-    // now that the client is created and configured, attempt to connect
-    err = client.Open()
-    if err != nil {
-        // error out if we failed to connect/open the device
-        // note: multiple Open() attempts can be made on the same client until
-        // the connection succeeds (i.e. err == nil), calling the constructor again
-        // is unnecessary.
-        // likewise, a client can be opened and closed as many times as needed.
-    }
+	// now that the client is created and configured, attempt to connect
+	err = client.Open()
+	if err != nil {
+		// error out if we failed to connect/open the device
+		// note: multiple Open() attempts can be made on the same client until
+		// the connection succeeds (i.e. err == nil), calling the constructor again
+		// is unnecessary.
+		// likewise, a client can be opened and closed as many times as needed.
+	}
 
-    type SolarData struct {
-        RealTime uint32 `json:"realtime"`
-        Today float32 `json:"today"`
-        Yesterday float32 `json:"yesterday"`
-        Month uint32 `json:"month"`
-        Year uint32 `json:"year"`
-    }
+	type SolarData struct {
+		RealTime  uint32  `json:"realtime"`
+		Today     float32 `json:"today"`
+		Yesterday float32 `json:"yesterday"`
+		Month     uint32  `json:"month"`
+		Year      uint32  `json:"year"`
+	}
 
-    var realtime   uint32
-    var today uint16
-    var yesterday uint16
-    var thismonth uint32
-    var thisyear uint32
-    
+	var realtime uint32
+	var today uint16
+	var yesterday uint16
+	var thismonth uint32
+	var thisyear uint32
+
 	today, _ = client.ReadRegister(3014, modbus.INPUT_REGISTER)
-    yesterday, _ = client.ReadRegister(3015, modbus.INPUT_REGISTER)
-    thismonth, _ = client.ReadUint32(3010, modbus.INPUT_REGISTER)
-    thisyear, _ = client.ReadUint32(3016, modbus.INPUT_REGISTER)
-    realtime, _ = client.ReadUint32(3004, modbus.INPUT_REGISTER)
-    client.Close()
-    
-    data := SolarData{}
-    data.RealTime = realtime
-    data.Today = float32(today) / 10
-    data.Yesterday = float32(yesterday) / 10
-    data.Month = thismonth
-    data.Year = thisyear
-    json, err := json.Marshal(data)
-    return string(json)
+	yesterday, _ = client.ReadRegister(3015, modbus.INPUT_REGISTER)
+	thismonth, _ = client.ReadUint32(3010, modbus.INPUT_REGISTER)
+	thisyear, _ = client.ReadUint32(3016, modbus.INPUT_REGISTER)
+	realtime, _ = client.ReadUint32(3004, modbus.INPUT_REGISTER)
+	client.Close()
+
+	data := SolarData{}
+	data.RealTime = realtime
+	data.Today = float32(today) / 10
+	data.Yesterday = float32(yesterday) / 10
+	data.Month = thismonth
+	data.Year = thisyear
+	json, err := json.Marshal(data)
+	return string(json)
 }
